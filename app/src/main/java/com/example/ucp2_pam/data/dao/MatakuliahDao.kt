68ad18674680fe591e1d87f04c57dd6d87f30d51
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-12-19 15:41:59 +0700

    koreksi pada MatakuliahDao

--- app/src/main/java/com/example/ucp2_pam/data/dao/MatakuliahDao.kt
+++ app/src/main/java/com/example/ucp2_pam/data/dao/MatakuliahDao.kt
@@ -5,21 +5,25 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.ucp2_pam.data.entity.Matakuliah
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MatakuliahDao {
     // memasukan data dengan mengambil tabel matakuliah
     // fungsi get all data
-    @Query("select * from matakuliah ORDER BY nama ASC")
+    @Query("select * from matakuliah ORDER BY kode ASC")
     fun getAllMatakuliah() : Flow<List<Matakuliah>>
+
     // getMatakuliah
-    @Query("select * from matakuliah WHERE kode = :kode")
-    fun getMatakuliah(nim: String) : Flow<Matakuliah>
+    @Query("select * from matakuliah WHERE kode =:kode")
+    fun getMatakuliah(kode: String) : Flow<Matakuliah>
+
     // deleteMatakuliah
     @Delete
     suspend fun deleteMatakuliah(matakuliah: Matakuliah)
     // updateMatakuliah
+
     @Update
     suspend fun updateMatakuliah(matakuliah: Matakuliah)
 
