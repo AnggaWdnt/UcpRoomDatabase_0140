20b1293f9ee03c757bb2122085c64e3513625f4b
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-12-19 15:58:05 +0700

    penambahan entitas class dosen pada KrsDatabase

---
+++ app/src/main/java/com/example/ucp2_pam/data/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.ucp2_pam.data
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UCP2_PAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+class UCP2_PAMTheme(function: @Composable () -> Unit) {
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UCP2_PAMTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/ucp2_pam/data/database/KrsDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.ucp2_pam.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2_pam.data.dao.MatakuliahDao
+import com.example.ucp2_pam.data.entity.Dosen
+import com.example.ucp2_pam.data.entity.Matakuliah
+
+@Database(entities = [Dosen::class, Matakuliah::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase(){
+    abstract fun  matakuliahdao(): MatakuliahDao
+
+    companion object{
+        @Volatile
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java, // Class database
+                    "KrsDatabase" // Nama database
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
