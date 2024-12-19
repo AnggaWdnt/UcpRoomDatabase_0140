b55a05b4a34f1822e41c1a2cc65a01214a159be6
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-12-19 14:59:53 +0700

    penambahan code interface pada DosenDao

--- app/src/main/java/com/example/ucp2_pam/data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2_pam/data/dao/DosenDao.kt
@@ -1,4 +1,16 @@
 package com.example.ucp2_pam.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface DosenDao {
+    @Query("SELECT * FROM dosen ORDER BY nama ASC")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    @Insert
+    suspend fun  insertDosen(
+        dosen: Dosen
+    )
 }
